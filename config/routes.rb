b972540ea0d615c5f14d7cfa87b4f93cdfe73b8f
Mark Chavez <markchav3z@gmail.com>
2015-05-16 07:55:14 +0800

    Change url routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "application#index"
 
-  devise_for :admins
+  devise_for :admins, path: "", path_names: { sign_in: "signin", sign_out: "signout" }
 
   namespace :admin do
     resource :dashboard, only: [:show]
